6ba9d97950bd970318640930cdb711f9fa904b5f
MoskaletsOleksandr <moskaletzsanych@gmail.com>
2023-10-17 16:54:13 +0300

    dnd-done

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -96,7 +96,8 @@ const updateCardById = async (req, res) => {
 const moveCardById = async (req, res, next) => {
   const { id } = req.user;
   const { cardId } = req.params;
-  const { newColumnId } = req.body;
+  const { newColumnId, newOrderInColumn } = req.body;
+  let updatedColumns;
 
   const cardToUpdate = await Card.findById(cardId);
   if (!cardToUpdate) {
@@ -104,41 +105,173 @@ const moveCardById = async (req, res, next) => {
   }
 
   const oldColumnId = cardToUpdate.columnId;
-  if (!newColumnId || newColumnId === oldColumnId) {
+  if (!newColumnId || !newOrderInColumn) {
     throw HttpError(400, 'No data to update');
   }
 
-  const newColumn = await Column.findById(newColumnId);
-  if (!newColumn) {
-    throw HttpError(404, 'The target column does not exist');
+  const oldOrderInColumn = cardToUpdate.order;
+  if (newColumnId === oldColumnId && newOrderInColumn === oldOrderInColumn) {
+    throw HttpError(400, 'No data to update');
   }
 
-  const oldColumnCardsToShift = await Card.find({
-    columnId: oldColumnId,
-    order: { $gt: cardToUpdate.order },
-  });
+  const oldColumnCardsQuantity = await getColomnCardsByOwnerAndColumnQuantity(
+    id,
+    oldColumnId
+  );
 
   const newColumnCardsQuantity = await getColomnCardsByOwnerAndColumnQuantity(
     id,
     newColumnId
   );
-  const cardToUpdateNewOrder = newColumnCardsQuantity + 1;
-  await cardToUpdate.updateOne({
-    columnId: newColumnId,
-    order: cardToUpdateNewOrder,
-  });
 
-  await Promise.all(
-    oldColumnCardsToShift.map(async (card) => {
-      card.order = card.order - 1;
+  if (newColumnId === oldColumnId && newOrderInColumn === 'null') {
+    if (oldOrderInColumn === oldColumnCardsQuantity) {
+      throw HttpError(400, 'No data to update');
+    }
+
+    const oldColumnCardsToShift = await Card.find({
+      columnId: oldColumnId,
+      order: { $gt: oldOrderInColumn },
+    });
+
+    const cardToUpdateNewOrder = oldColumnCardsQuantity;
+    await cardToUpdate.updateOne({
+      order: cardToUpdateNewOrder,
+    });
+
+    await Promise.all(
+      oldColumnCardsToShift.map(async (card) => {
+        card.order = card.order - 1;
+        await card.save();
+      })
+    );
+
+    const oldColumnCards = await getColomnCardsByOwnerAndColumn(
+      id,
+      oldColumnId
+    );
+
+    updatedColumns = [oldColumnCards];
+  }
+
+  if (newColumnId === oldColumnId && newOrderInColumn !== 'null') {
+    if (newOrderInColumn > oldColumnCardsQuantity) {
+      throw HttpError(
+        400,
+        'New order is greater than the number of cards in the column'
+      );
+    }
+
+    const range =
+      oldOrderInColumn < newOrderInColumn
+        ? { $gt: oldOrderInColumn, $lte: newOrderInColumn }
+        : { $gte: newOrderInColumn, $lt: oldOrderInColumn };
+
+    const cardsInRange = await Card.find({
+      columnId: oldColumnId,
+      order: range,
+    });
+
+    await cardToUpdate.updateOne({ order: newOrderInColumn });
+
+    const updates = cardsInRange.map(async (card) => {
+      if (oldOrderInColumn < newOrderInColumn) {
+        card.order -= 1;
+      } else {
+        card.order += 1;
+      }
       await card.save();
-    })
-  );
+    });
 
-  const oldColumnCards = await getColomnCardsByOwnerAndColumn(id, oldColumnId);
-  const newColumnCards = await getColomnCardsByOwnerAndColumn(id, newColumnId);
+    await Promise.all(updates);
 
-  const updatedColumns = [oldColumnCards, newColumnCards];
+    const oldColumnCards = await getColomnCardsByOwnerAndColumn(
+      id,
+      oldColumnId
+    );
+
+    updatedColumns = [oldColumnCards];
+  }
+
+  if (newColumnId !== oldColumnId && newOrderInColumn === 'null') {
+    const oldColumnCardsToShift = await Card.find({
+      columnId: oldColumnId,
+      order: { $gt: oldOrderInColumn },
+    });
+
+    await Promise.all(
+      oldColumnCardsToShift.map(async (card) => {
+        card.order = card.order - 1;
+        await card.save();
+      })
+    );
+
+    const cardToUpdateNewOrder = newColumnCardsQuantity + 1;
+    await cardToUpdate.updateOne({
+      order: cardToUpdateNewOrder,
+      columnId: newColumnId,
+    });
+
+    const oldColumnCards = await getColomnCardsByOwnerAndColumn(
+      id,
+      oldColumnId
+    );
+    const newColumnCards = await getColomnCardsByOwnerAndColumn(
+      id,
+      newColumnId
+    );
+
+    updatedColumns = [oldColumnCards, newColumnCards];
+  }
+
+  if (newColumnId !== oldColumnId && newOrderInColumn !== 'null') {
+    if (newOrderInColumn > newColumnCardsQuantity) {
+      throw HttpError(
+        400,
+        'New order is greater than the number of cards in the column'
+      );
+    }
+
+    const oldColumnCardsToShift = await Card.find({
+      columnId: oldColumnId,
+      order: { $gt: oldOrderInColumn },
+    });
+
+    await Promise.all(
+      oldColumnCardsToShift.map(async (card) => {
+        card.order = card.order - 1;
+        await card.save();
+      })
+    );
+
+    const newColumnCardsToShift = await Card.find({
+      columnId: newColumnId,
+      order: { $gte: newOrderInColumn },
+    });
+
+    await Promise.all(
+      newColumnCardsToShift.map(async (card) => {
+        card.order = card.order + 1;
+        await card.save();
+      })
+    );
+
+    await cardToUpdate.updateOne({
+      order: newOrderInColumn,
+      columnId: newColumnId,
+    });
+
+    const oldColumnCards = await getColomnCardsByOwnerAndColumn(
+      id,
+      oldColumnId
+    );
+    const newColumnCards = await getColomnCardsByOwnerAndColumn(
+      id,
+      newColumnId
+    );
+
+    updatedColumns = [oldColumnCards, newColumnCards];
+  }
 
   res.status(200).json(updatedColumns);
 };
